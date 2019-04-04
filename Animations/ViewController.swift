001fe524e77059102b6361113d1181f61114ab09
Gabriel Henrique <gabhenrique0@gmail.com>
2019-04-04 11:35:07 -0300

    Create an animated progress bar inside our project

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -12,7 +12,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var viewDeFora: UIView!
     @IBOutlet weak var boxView: UIView!
     @IBOutlet weak var boxHeightConstraint: NSLayoutConstraint!
+    
     @IBOutlet weak var customButton: UIButton!
+    
+    @IBOutlet weak var loadingViewDeFora: UIView!
+    @IBOutlet weak var loadingViewDeDentro: UIView!
     
     var isExpanded: Bool = false
     
@@ -20,12 +24,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         boxView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(gesture:))))
+        
+        loadingViewDeFora.addGestureRecognizer(UITapGestureRecognizer(target: self
+            , action: #selector(loadingViewTap(_ :))))
     }
+    
+    @objc func loadingViewTap(_ gesture: UITapGestureRecognizer?) {
+        UIView.animate(withDuration: 0.5, delay: 0.3, options: [.curveEaseIn], animations: {
+            self.loadingViewDeDentro.frame = CGRect(x: 0, y: 0, width: self.loadingViewDeFora.frame.size.width, height: 15)
+        }) { (finished) in
+            UIView.animate(withDuration: 0.5, delay: 0.5, options: [.curveEaseIn], animations: {
+                self.loadingViewDeDentro.frame = CGRect(x: UIScreen.main.bounds.width, y: 0, width: 30, height: 15)
+            }, completion: { finished in
+                self.loadingViewDeDentro.frame = CGRect(x: -100, y: 0, width: 0, height: 15)
+                self.loadingViewTap(nil)
+            })
+        }
+    }	
     
     @objc func handleTap(gesture: UITapGestureRecognizer) {
         self.isExpanded = !isExpanded
         
-        let height: CGFloat = isExpanded ? 300 : 50
+        let height: CGFloat = isExpanded ? 200 : 50
         
         UIView.animate(withDuration: 0.5) {
             self.boxHeightConstraint.constant = height
